54553bbe6f40c0fc63e4a985347e23e47b239d77
Xingfu <iceeric02@gmail.com>
2023-08-22 10:20:46 +0200

    adding 2017 test ac configuration file

---
+++ Signal/config_accat_2017.py
@@ -0,0 +1,26 @@
+# Config file: options for signal fitting
+
+_year = '2017'
+
+signalScriptCfg = {
+    # Setup
+    'inputWSDir':'/eos/user/x/xisu/WorkSpace/VH_AC_Analysis/flashgg_output/UL_dataset/outputwsforfinal/UL_17/signal',
+    'procs':'auto',
+    'cats':'auto',
+    'ext':'dcb_newcat_%s'%_year,
+    'analysis':'STXS', # To specify which replacement dataset mapping (defined in ./python/replacementMap.py)
+    'year':'%s'%_year, # Use 'combined' if merging all years: not recommended
+    'massPoints':'120,125,130',
+    'xvar': 'CMS_hgg_mass',
+    'outdir': 'plots',
+
+    #Photon shape systematics  
+    'scales':'HighR9EB,HighR9EE,LowR9EB,LowR9EE,Gain1EB,Gain6EB', # separate nuisance per year
+    'scalesCorr':'MaterialCentralBarrel,MaterialOuterBarrel,MaterialForward,FNUFEE,FNUFEB,ShowerShapeHighR9EE,ShowerShapeHighR9EB,ShowerShapeLowR9EE,ShowerShapeLowR9EB', # correlated across years
+    'scalesGlobal':'NonLinearity,Geant4', # affect all processes equally, correlated across years
+    'smears':'HighR9EBPhi,HighR9EBRho,HighR9EEPhi,HighR9EERho,LowR9EBPhi,LowR9EBRho,LowR9EEPhi,LowR9EERho', # separate nuisance per year
+
+    # Job submission options
+    'batch':'condor', # ['condor','SGE','IC','Rome','local']
+    'queue':'espresso'
+}
