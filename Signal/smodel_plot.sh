88aaad4394bf4ef321b5efba03791d2cb848e4fa
Xingfu <iceeric02@gmail.com>
2023-08-23 04:20:13 +0200

    script for plotting signal model

---
+++ Signal/smodel_plot.sh
@@ -0,0 +1,5 @@
+python RunPlotter.py --procs all --years 2017 --cats allpt_VH_MET_Tag0 --ext dcb_newcat_2017 --translateCats tools/translate_map.json
+python RunPlotter.py --procs all --years 2017 --cats allpt_VH_MET_Tag1 --ext dcb_newcat_2017 --translateCats tools/translate_map.json
+python RunPlotter.py --procs all --years 2017 --cats allpt_VH_MET_Tag2 --ext dcb_newcat_2017 --translateCats tools/translate_map.json
+python RunPlotter.py --procs all --years 2017 --cats allpt_VH_MET_Tag3 --ext dcb_newcat_2017 --translateCats tools/translate_map.json
+python RunPlotter.py --procs all --years 2017 --cats allpt_VH_MET_Tag4 --ext dcb_newcat_2017 --translateCats tools/translate_map.json
